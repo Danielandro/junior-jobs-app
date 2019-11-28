a83d6a5bffcfbe3e5baeeb261db6df744611de9f
Danielandro <mtme13@hotmail.com>
2019-11-28 15:37:31 +0000

    Display mock jobs on page

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import Jobs from './Jobs';
+
+const mockJobs = [
+  { title: 'Junior React Developer', company: "DAZN" },
+  { title: 'Junior Full Stack Developer', company: "ClearScore" },
+  { title: 'Junior Java Developer', company: "Amazon" }
+]
 
 function App() {
   return (
     <div className="App">
+      <Jobs jobs={mockJobs} />
     </div>
   );
 }
