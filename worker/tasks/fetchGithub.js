155d71a43c8c37c5793b65b3d926531dea90d738
Danielandro <mtme13@hotmail.com>
2019-12-01 23:56:16 +0000

    FetchGithub module fetches all results

--- worker/tasks/fetchGithub.js
+++ worker/tasks/fetchGithub.js
@@ -1,12 +1,31 @@
 const fetch = require('node-fetch');
 
-const allJobs = [];
-const baseUrl = "https://jobs.github.com/positions.json?page=1&description=javascript";
+const allJobs = []; // job results
+const baseUrl = "https://jobs.github.com/positions.json?description=javascript&page="; // github jobs API
+let pageNumber = 0; // results page
+let numOfJobs = 1;
+
+/*
+- send fetch request to first page (page 0) x
+- get back results -> array of job objects x
+- add results to allJobs x
+- print how many jobs found e.g 50 jobs found x
+- send request to next page (page 1)
+- keep going until a page returns no results 
+- print total number of jobs found
+*/
 
 async function fetchGithub() {
-  let jobs = await fetch(baseUrl)
-    .then(res => res.json());
-  console.log(jobs.length);
+  while (numOfJobs !== 0) {
+    let foundJobs = await fetch(`${baseUrl}${pageNumber}`)
+      .then(res => res.json());
+    numOfJobs = foundJobs.length;
+    allJobs.push(...foundJobs);
+    console.log(`${numOfJobs} jobs`);
+    pageNumber++;
+  }
+
+  console.log(`Total number of jobs found: ${allJobs.length}`);
 }
 
 fetchGithub();
