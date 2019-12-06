a00c6785248bca93b3b250ec848f392333fb6d87
Danielandro <mtme13@hotmail.com>
2019-12-06 10:54:09 +0000

    Cached jobs render to page

--- client/src/App.js
+++ client/src/App.js
@@ -2,24 +2,17 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Jobs from './Jobs';
 
-const mockJobs = [
-  { title: 'Junior React Developer', company: "DAZN" },
-  { title: 'Junior Full Stack Developer', company: "ClearScore" },
-  { title: 'Junior Java Developer', company: "Amazon" }
-]
-
 const jobApiUrl = 'http://localhost:3001/jobs';
-const fetchJobs = async () => {
+const fetchJobs = async (updateCallback) => {
   const res = await fetch(jobApiUrl);
   const json = await res.json();
-
-  console.log({ json });
+  updateCallback(json);
 }
 
 function App() {
   const [jobList, updateJobs] = useState([])
   useEffect(() => {
-    fetchJobs();
+    fetchJobs(updateJobs);
   }, [])
 
   return (
