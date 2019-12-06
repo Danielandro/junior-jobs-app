58724686657e50cc74c2a16f4aafc61ef3679ba5
Danielandro <mtme13@hotmail.com>
2019-12-06 08:46:04 +0000

    Github jobs are persisted in Redis

--- worker/tasks/fetchGithub.js
+++ worker/tasks/fetchGithub.js
@@ -1,11 +1,18 @@
 const fetch = require('node-fetch');
+const redis = require('redis');
+const client = redis.createClient();
+const { promisify } = require('util');
+const setAsync = promisify(client.set).bind(client);
 
 const allJobs = []; // job results
 const baseUrl = "https://jobs.github.com/positions.json?description=javascript&page="; // github jobs API
 
+// fetch jobs from each results page
 async function fetchGithub() {
-  let pageNumber = 0; // results page
+  let pageNumber = 0;
   let numOfJobs = 1;
+
+  console.log("Fetching Github...")
 
   while (numOfJobs !== 0) {
     const res = await fetch(`${baseUrl}${pageNumber}`)
@@ -18,6 +25,13 @@ async function fetchGithub() {
   }
 
   console.log(`Total number of jobs found: ${allJobs.length}`);
+  // store jobs in redis 
+  client.on("error", (err) => {
+    console.log(`Error ${err}`)
+  })
+
+  const success = await setAsync('github', JSON.stringify(allJobs));
+  console.log({ success });
 
 }
 
